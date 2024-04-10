a6f457be3a20c73ed490b263685e1c5ea787cc79
DoLT <dolt@guide.inc>
2024-04-10 13:31:08 +0900

    fix: crash main window when close child window

--- windows/window_manager_plugin.cpp
+++ windows/window_manager_plugin.cpp
@@ -125,6 +125,9 @@ void WindowManagerPlugin::_EmitEvent(std::string eventName) {
   flutter::EncodableMap args = flutter::EncodableMap();
   args[flutter::EncodableValue("eventName")] =
       flutter::EncodableValue(eventName);
+  channel = std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+      registrar->messenger(), "window_manager",
+      &flutter::StandardMethodCodec::GetInstance());
   channel->InvokeMethod("onEvent",
                         std::make_unique<flutter::EncodableValue>(args));
 }
